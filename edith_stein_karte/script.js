01754a2b9f8216a7554ef44ca375eb0ed3e82bd9
JoshR <j.roe@karmel.at>
2026-07-21 10:30:34 +0200

    updating color markers

--- edith_stein_karte/script.js
+++ edith_stein_karte/script.js
@@ -1,7 +1,7 @@
 'use strict'
             let src = 'edith_stein_orte.json';
 
-            var map = L.map('map').setView([48.16117344, 16.32525916], 13);
+            var map = L.map('map').setView([51.2, 12.5], 6);
                     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
                 maxZoom: 19,
                 attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
@@ -41,7 +41,16 @@ function displayData(element) {
     let addr = element.properties.ADRESSE;
     let bezAddr = `${bez}<br>${addr}`;
 
-    const marker = L.marker([lat, long], { title: bez }).addTo(map);
+    const marker = L.circleMarker([lat, long], {
+        radius: 8,
+        fillColor: getMarkerColor(element.type),
+        color: '#333',
+        weight: 1,
+        fillOpacity: 0.8
+    }).addTo(map);
+
+
+  //  const marker = L.marker([lat, long], { title: bez }).addTo(map);
     marker.bindPopup(bezAddr);
 }
             //function auslösen beim laden der Seite
